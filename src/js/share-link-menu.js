f64f00f26055248ce224fcfa5d81ae5f6a916f0e
7Kart <artgor92@gmail.com>
2020-06-09 23:20:20 +0300

    add share menu

--- src/js/share-link-menu.js
+++ src/js/share-link-menu.js
@@ -1,20 +1,16 @@
 console.log('init share link');
 import $ from "jquery"
 
-// import $ from "jquery"
-
-
 export default function (selector) {
-
     $(document).on('click', selector, (e) => {
         const post = $(e.currentTarget).data();
         if (post) {
             const shareMenu = $(`#link-post-${post.id}`);
-
             shareMenu.toggleClass("social-link-container-open")
-
-            console.log('shareMenu', shareMenu);
         }
     });
 
+    $("#social-item-link").click(()=>{
+        console.log(' link ready to copy');
+    })
 }
