2c26890b0526403a74517fc7dcd1452ec575ffae
7Kart <artgor92@gmail.com>
2020-06-24 22:27:12 +0300

    fix font and remove page transition animation

--- src/js/index.js
+++ src/js/index.js
@@ -10,25 +10,25 @@ const navMenu = new NavMenu(".navigation-menu", "navigation-menu-open", ".hambur
 navMenu.runMenu();
 
 //init page animation
-let links = document.querySelectorAll('a');
-if (links) {
-    links.forEach(link => {
-        if (!link.classList.contains("ignore-page-transition")) {
-            link.onclick = (e) => {
-                let body = document.querySelector("main");
-                e.preventDefault();
-                console.log('clicl link');
-                setTimeout(function () {
-                    if (body.classList.contains("fade-out")) {
-                        if (!e.srcElement.parentElement.href) {
-                            window.location = e.srcElement.href;
-                        } else {
-                            window.location = e.srcElement.parentElement.href;
-                        }
-                    }
-                }, 300);
-                body.classList.add("fade-out")
-            }
-        }
-    });
-}
+// let links = document.querySelectorAll('a');
+// if (links) {
+//     links.forEach(link => {
+//         if (!link.classList.contains("ignore-page-transition")) {
+//             link.onclick = (e) => {
+//                 let body = document.querySelector("main");
+//                 e.preventDefault();
+//                 console.log('clicl link');
+//                 setTimeout(function () {
+//                     if (body.classList.contains("fade-out")) {
+//                         if (!e.srcElement.parentElement.href) {
+//                             window.location = e.srcElement.href;
+//                         } else {
+//                             window.location = e.srcElement.parentElement.href;
+//                         }
+//                     }
+//                 }, 300);
+//                 body.classList.add("fade-out")
+//             }
+//         }
+//     });
+// }
