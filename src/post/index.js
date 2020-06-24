65cc975a24ea78e5d94e59303cb45e4fee8fac91
7Kart <artgor92@gmail.com>
2020-06-24 21:22:38 +0300

    update fonts

--- src/post/index.js
+++ src/post/index.js
@@ -2,22 +2,22 @@ import '../js';
 import "../assets/scss/main.scss";
 import "../assets/scss/post.scss";
 
-import $ from "jquery";
+// import $ from "jquery";
 import shareMenu from '../js/share-link-menu';
 
-const url = "http://teawall/wp-admin/admin-ajax.php?action=getPost";
+// const url = "http://teawall/wp-admin/admin-ajax.php?action=getPost";
 // console.log('init post script');
 
 const postContent = document.getElementsByClassName("post-content");
 
 
-$.ajax({
-    url: url,
-    method: "GET",
+// $.ajax({
+//     url: url,
+//     method: "GET",
 
-}).done(data => {
-    $('.posts-container').prepend(data);
-});
+// }).done(data => {
+//     $('.posts-container').prepend(data);
+// });
 
 shareMenu(".share-button", "social-link-container-open")
 
